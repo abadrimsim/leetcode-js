06c43e74cff0c0d57a43434099b521ee0fe17933
Angel <abadrimsim@gmail.com>
2022-06-29 01:48:03 +0800

    Initial commit

---
+++ math/PlusOne/PlusOne.js
@@ -0,0 +1,29 @@
+/**
+ * Source : https://leetcode.com/problems/plus-one/
+ *
+ *
+ *
+ *
+ * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+ * The digits are ordered from most significant to least significant in left-to-right order.
+ * The large integer does not contain any leading 0's.
+ *
+ * Increment the large integer by one and return the resulting array of digits.
+ *
+ *  Input: digits = [1,2,3]
+ *  Output: [1,2,4]
+ *  Explanation: The array represents the integer 123.
+ *  Incrementing by one gives 123 + 1 = 124.
+ *  Thus, the result should be [1,2,4].
+ *
+ */
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+const plusOne = function (digits) {
+	const num = BigInt(digits.join('')) + BigInt(1);
+	return num.toString().split('');
+};
